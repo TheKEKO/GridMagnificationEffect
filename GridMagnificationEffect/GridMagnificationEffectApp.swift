0b05864e6ea0f055b4148f8cb4325f3f6faae733
Aleksandr F <a7214370@gmail.com>
2022-09-02 21:26:36 +0300

    Initial Commit

---
+++ GridMagnificationEffect/GridMagnificationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridMagnificationEffectApp.swift
+//  GridMagnificationEffect
+//
+//  Created by Aleksandr F. on 02.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct GridMagnificationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
